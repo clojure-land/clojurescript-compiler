723f009b5ab16cdd8c3d4def6bb8c3df03fb6f2e
Arne Brasseur <arne@arnebrasseur.net>
2016-06-16 21:47:59 +0200

    18

--- clj/build.clj
+++ clj/build.clj
@@ -1,6 +1,7 @@
 (require '[cljs.build.api :as cljs])
 
 (def compiler-options {:output-to "compiled.js"
+                       :source-map "compiled.js.map"
                        :optimizations :whitespace})
 
 (cljs/build "cljs" compiler-options)
